7a7f8ccd646df9fc3f42401094c2ddcee7593128
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-22 10:36:56 +0000

    save

---
+++ week02/hello2.py
@@ -0,0 +1,10 @@
+# This program prints  reads in a person's name and prints out hello that persons name.
+# Author Jennifer Ibanez Cano
+
+name = input("Enter your name: ")
+print('Hello' + name)
+
+# The program will say nice to meet you after saying hello
+
+name = input("Enter your name: ")
+print('Hello' + name + '\nNice to meet you')
