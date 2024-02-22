3eaae52387bb7450e93c3936ad10a4c40c1df6ef
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-22 13:00:43 +0000

    save

---
+++ week03/div.py
@@ -0,0 +1,9 @@
+# This program will reads to number.
+# Also divides the first number by the second and will outputs the integrer answer and remainder.
+# Author Jennifer Ibanez Cano
+
+a = int(input("Enter first number: "))
+b = int(input("Enter the number you want to divide by: "))
+answer= int(a//b)
+remainder = a%b
+print(" {} divided by {} is {} with remainder {} ".format(a, b, answer, remainder))
