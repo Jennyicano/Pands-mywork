80d9c2d91a82035b2c972c6bfba339252ceb9508
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-22 12:52:53 +0000

    save

---
+++ week03/sub.py
@@ -0,0 +1,10 @@
+# This program will subtract on number from another
+# Author Jennifer Ibanez Cano
+
+# input reads in a string so we need to convert it into an int
+# then we cna perform mathematical operations
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+answer= a - b
+print(" {} minus {} is {} ".format (a, b, answer))
