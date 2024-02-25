8a08be1d0d72e80e405050ee721bf553d6fccb04
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-25 14:54:19 +0000

    save

---
+++ week04-flow/lab4.1-isEven.py
@@ -0,0 +1,12 @@
+# This program asks the usesr to enter in a number.
+# The program will tell the user if the number is even or odd.
+# Using if and else
+
+# Author Jennifer Ibanez Cano
+
+number = int(input("Enter a number: "))
+
+if (number % 2) == 0:
+    print (f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
