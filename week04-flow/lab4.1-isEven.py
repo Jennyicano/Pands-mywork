9e700f3319585a41239c0bcd62e427ee92a0456f
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-25 14:54:44 +0000

    save

--- week04-flow/lab4.1-isEven.py
+++ week04-flow/lab4.1-isEven.py
@@ -1,4 +1,4 @@
-# This program asks the usesr to enter in a number.
+# This program asks the user to enter in a number.
 # The program will tell the user if the number is even or odd.
 # Using if and else
 
